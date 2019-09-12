0a099bf1db25ff10396d28485822435f1a26d003
kotakosu <11920513k>
2019-09-12 20:09:14 +0900

    message_edit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,34 +1,45 @@
-$(function(){
-  function buildHTML(messages){
-    var html = `<div class="message" data-message-id=${message.id}>
-         <div class="up-message__user-name">
-          ${message.user_name}
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+      `<div class="message" data-message-id=${message.id}>
+         <div class="up-message">
+           <div class="up-message__user-name">
+             ${message.user_name}
+           </div>
+           <div class="up-message__date">
+             ${message.date}
+           </div>
          </div>
-         <div class="up-message__date">
-          ${message.date}
+         <div class="return-message">
+             ${message.content}
+           </p>
          </div>
-         </div>`
-    return html;
-  }
-  
+         <img src=${message.image} >
+       </div>`
+     return html;
+   };
+ }
 $('.js-form').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+    $('form')[0].reset();
   })
-.done(function(data){
-  var html = buildHTML(data);
-       $('.messages').append(html);
-})
-.fail(function(){
-  alert('error');
-})
-})
+   .fail(function(){
+     alert('error');
+   });
+ });
 });
