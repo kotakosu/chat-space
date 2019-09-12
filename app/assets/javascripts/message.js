449932c69ee65f5a2b9fe9c31f6b6dc0a254ff11
kotakosu <11920513k>
2019-09-12 19:18:29 +0900

    messages_create

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,41 @@
-$('.js-form').on('submit', function(){
-  console.log('hoge');
+$(function(){
+  function buildHTML(messages){
+    var html = `<div class="message" data-message-id=${message.id}>
+         <div class="up-message__user-name">
+          ${message.user_name}
+         </div>
+         <div class="up-message__date">
+          ${message.date}
+         </div>
+         </div>`
+    return html;
+  }
+  `<div class="message" data-message-id=${message.id}>
+         <div class="up-message__user-name">
+          ${message.user_name}
+         </div>
+         <div class="up-message__date">
+          ${message.date}
+         </div>
+         </div>`
+$('.js-form').on('submit', function(e){
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action')
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+.done(function(data){
+  var html = buildHTML(data);
+       $('.messages').append(html);
+})
+.fail(function(){
+  alert('error');
+})
+})
 });
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,3 @@
+json.id @message.id
+json.user_name @message.user.name
+json.date @message.created_at.strftime
